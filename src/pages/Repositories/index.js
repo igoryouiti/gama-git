b7634d51dbbd294e4930c3b6588a8dd01ad8425e
Igor Youiti Sato <igoryouiti@hotmail.com>
2021-07-07 15:12:39 -0300

    first commit

---
+++ src/pages/Repositories/index.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+
+import * as S from './styled';
+
+export default function Repositories() {
+
+  const history = useHistory();
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    let repositoriesName = localStorage.getItem('repositoriesName');
+
+    if (repositoriesName !== null) {
+      repositoriesName = JSON.parse(repositoriesName);
+      setRepositories(repositoriesName);
+      localStorage.clear();
+    } else {
+      history.push('/');
+    }
+  }, []);
+
+
+  return (
+    <S.Container>
+      <S.Title>Repositories</S.Title>
+      <S.List>
+        {repositories?.map((repository, index) => {
+          return (
+            <S.ListItem key={index}>Repositório: {repository}</S.ListItem>
+          )
+        })}
+      </S.List>
+      <S.LinkHome to="/">Voltar</S.LinkHome>
+    </S.Container>
+  );
+}
+
